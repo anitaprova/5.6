498f6164a0b681ba9afb1372d9384f5b60f2ee3e
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-29 12:14:48 -0400

    finished

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	
+	int size = str.size() - 1;
+	if (size % 2 == 0) {
+		cout << str.substr(size/2, size/2 - 1) << endl;	
+	}
+	else {
+		cout << str.substr( (int)size/2, (int)size/2) << endl;
+	}
+
+	return 0;
+}

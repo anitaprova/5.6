5e1c5b5085cf6d2b4b8b4f11e4f249c64b3114f0
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-29 13:41:38 -0400

    fixed bug

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ int main() {
 	string str;
 	cin >> str;
 	
-	int size = str.size() - 1;
-	if (size % 2 == 0) {
-		cout << str.substr(size/2, size/2 - 1) << endl;	
+	int size = str.size();
+	if (size % 2 == 0) { //even
+		cout << str.substr((int)(size-1)/2, 2) << endl;	
 	}
 	else {
-		cout << str.substr( (int)size/2, (int)size/2) << endl;
+		cout << str.substr((size-1)/2, 1) << endl;
 	}
 
 	return 0;

b547439615a948bd887194decd5ae7e6c8172cbf
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-06 10:23:06 -0400

    added function

--- main.cpp
+++ main.cpp
@@ -3,17 +3,23 @@
 
 using namespace std;
 
-int main() {
-	string str;
-	cin >> str;
-	
+string middle(string str) {
 	int size = str.size();
+	
 	if (size % 2 == 0) { //even
 		cout << str.substr((int)(size-1)/2, 2) << endl;	
 	}
 	else {
 		cout << str.substr((size-1)/2, 1) << endl;
 	}
+	return "";
+}
+
+int main() {
+	string s;
+	cin >> s;
+	
+	middle(s);
 
 	return 0;
 }
